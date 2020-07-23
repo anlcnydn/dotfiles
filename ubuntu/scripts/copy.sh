2216819ea81b165ce5aa91f00140d228e29aca2e
Anil Can Aydin <anlcnydn@gmail.com>
2020-07-23 18:29:24 +0300

    zsh installation added

---
+++ ubuntu/scripts/copy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Lets say some configuration files are copied."
+
+# up from scripts
+cd ..
+
+cp .zshrc ${HOME}
---
+++ ubuntu/scripts/zsh/oh-my-zsh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Installing oh-my-zsh..."
+
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
